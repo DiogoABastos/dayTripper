515a511465c2c6c6f0a327a5ad0bc9e4ce499ac5
Miguel Cabral <m.r.cabral@hotmail.com>
2020-03-03 11:18:37 +0000

    navbar transparent and logo

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,1 +1,6 @@
 import "bootstrap";
+
+
+const pagesHome = document.querySelector(".home");
+const navbarTarget = document.querySelector(".navbar-lewagon");
+if(pagesHome) navbarTarget.classList.add("nav-transparent");
