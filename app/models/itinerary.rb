a136cbd493ea797e5b65cdb4735eaa6c01be2967
Stefan Sideris <stefanpaulsideris@gmail.com>
2020-03-06 07:10:26 +0000

    added code for cloning, still need to enable button and path

--- app/models/itinerary.rb
+++ app/models/itinerary.rb
@@ -18,4 +18,29 @@ class Itinerary < ApplicationRecord
         }
       end
   end
+
+  def clone_with_locations(user)
+    attr = self.attributes
+    attr = attr.except("id", "user_id", "created_at", "updated_at")
+    new_itinerary = Itinerary.new(attr)
+    new_itinerary.user = user
+    new_itinerary.save
+
+    self.locations.each do |loc|
+      loc_attr = loc.attributes
+      loc_attr = loc_attr.except("id", "created_at", "updated_at")
+      new_location = Location.new(loc_attr)
+      new_location.save
+
+      loc.itinerary_locations.each do |itinerary_loc|
+        new_itinerary_location = ItineraryLocation.new
+        new_itinerary_location.location = new_location
+        new_itinerary_location.itinerary = new_itinerary
+        new_itinerary_location.save
+      end
+    end
+    new_itinerary
+  end
+
 end
+
