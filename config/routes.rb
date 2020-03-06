67a8db31f7dc325e6a193dfa73482a49f1cba9ee
Stefan Sideris <stefanpaulsideris@gmail.com>
2020-03-06 16:30:27 +0000

    updated the profile page

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   get '/profile', to: 'pages#profile'
+  patch '/profile', to: 'pages#update_profile'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :itineraries do
