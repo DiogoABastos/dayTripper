be4889337b1a28b87afef343d464fb0113b2e9f7
Miguel Cabral <m.r.cabral@hotmail.com>
2020-03-10 10:31:34 +0000

    it's good

--- app/models/itinerary.rb
+++ app/models/itinerary.rb
@@ -24,19 +24,26 @@ class Itinerary < ApplicationRecord
     new_itinerary.name = "**COPY OF " + new_itinerary.name + "**"
     new_itinerary.save
 
-    self.locations.each do |loc|
+    locs = self.locations.map do |loc|
       loc_attr = loc.attributes
       loc_attr = loc_attr.except("id", "created_at", "updated_at")
       new_location = Location.new(loc_attr)
+      loc.tag_list.each do |tag|
+        new_location.tag_list.add(tag)
+      end
       new_location.save
 
-      loc.itinerary_locations.each do |itinerary_loc|
-        new_itinerary_location = ItineraryLocation.new
-        new_itinerary_location.location = new_location
-        new_itinerary_location.itinerary = new_itinerary
-        new_itinerary_location.save
-      end
+      ItineraryLocation.create(location: new_location, itinerary: new_itinerary)
+
+      # loc.itinerary_locations.each do |itinerary_loc|
+      #   new_itinerary_location = ItineraryLocation.new
+      #   new_itinerary_location.location = new_location
+      #   new_itinerary_location.itinerary = new_itinerary
+      #   new_itinerary_location.save
+      # end
     end
+    # new_itinerary.locations << locs
+
     new_itinerary
   end
 
